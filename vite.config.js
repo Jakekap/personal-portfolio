19141bbbe2ee8387713f21b0bc31eb5b87529794
Christian Andrade <ce.andrade.p@gmail.com>
2023-11-28 19:18:14 -0500

    :bug: Fix vite migration plugin problems
    add swc emotion plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +12,7 @@ export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
+      plugins: [['@swc/plugin-emotion', {}]],
     }),
   ],
   resolve: {},
