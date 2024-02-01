401450b5fcb5759502c8af624fb057f2e4ebb4fd
Christian Andrade <ce.andrade.p@gmail.com>
2024-02-01 14:23:18 -0500

    :sparkles: Add hamburger menu
    add hamburger menu on mobile view

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,13 @@ import Typography from '../Typography'
 import { useNavigate, Link } from 'react-router-dom'
 import { theme } from '../../constants/theme'
 import DropdownMenu from '../DropdownMenu'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrClose } from 'react-icons/gr'
+import { useState } from 'react'
 
 const NavContainer = styled.div`
   display: flex;
+  position: relative;
   box-sizing: border-box;
   background-color: transparent;
   justify-content: space-between;
@@ -44,6 +48,66 @@ const TitleContainer = styled.button`
   gap: 1rem;
 `
 
+const HamburgerMenu = styled.div`
+  display: none;
+  @media (max-width: 1200px) {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 0;
+    margin-right: 10%;
+  }
+  @media only screen and (max-width: 600px) {
+    margin-right: 20px;
+  }
+`
+
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  svg {
+    cursor: pointer;
+  }
+`
+
+const SideContent = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2rem;
+  a {
+    font-size: 2.5rem;
+  }
+`
+
+const SideMenu = styled.div`
+  display: none;
+  @media (max-width: 1200px) {
+    display: block;
+    background-color: rgba(255, 255, 255, 0.6);
+    backdrop-filter: blur(10px);
+    z-index: 100;
+    position: fixed;
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 100vh;
+    transition: 0.5s;
+    padding: 20px 10% 0;
+    @media only screen and (max-width: 600px) {
+      padding: 20px 20px 0;
+    }
+    box-sizing: border-box;
+    ${({ isOpen }) =>
+      isOpen &&
+      `
+      left: 0;
+      transition: 0.5s;
+    `}
+  }
+`
+
 const Logo = styled('img')({
   width: '50px',
 })
@@ -52,6 +116,7 @@ function Header() {
   const handleRedirect = () => {
     navigate('/')
   }
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header>
       <IconContext.Provider value={{ size: '2rem' }}>
@@ -74,6 +139,72 @@ function Header() {
               About me
             </Link>
           </Navbar>
+          <HamburgerMenu onClick={() => setIsOpen(true)}>
+            <GiHamburgerMenu size='2.5rem' color='#464646' />
+          </HamburgerMenu>
+          <SideMenu isOpen={isOpen}>
+            <CloseContainer onClick={() => setIsOpen(false)}>
+              <GrClose size='3rem' color='#464646' />
+            </CloseContainer>
+            <SideContent>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/'
+              >
+                Home
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/projects'
+              >
+                Web projects
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/videogames'
+              >
+                Videogames
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/3dmodels'
+              >
+                3D Models
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/pixelart'
+              >
+                Pixel art
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/illustrations'
+              >
+                Illustrations
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/animations'
+              >
+                Animations
+              </Link>
+              <Link
+                onClick={() => setIsOpen(false)}
+                css={linkStyle}
+                to='/about-me'
+              >
+                About me
+              </Link>
+            </SideContent>
+          </SideMenu>
         </NavContainer>
       </IconContext.Provider>
     </header>
--- src/index.jsx
+++ src/index.jsx
@@ -85,6 +85,17 @@ const router = createBrowserRouter([
       </Layout>
     ),
   },
+  {
+    path: '*',
+    element: (
+      <Layout>
+        <div>
+          <h1>Error 404 - Página no encontrada</h1>
+          <p>La página que estás buscando no existe.</p>
+        </div>
+      </Layout>
+    ),
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
